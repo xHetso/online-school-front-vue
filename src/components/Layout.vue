58a46eff17a7fc5df2b50b6addb6c5333ade930d
Hetso <datacener23491248@gmail.com>
2024-03-24 17:09:03 +0500

    Add dynamic page title in Layout.vue

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,11 +1,17 @@
 <script setup>
+import { useRoute } from 'vue-router'
 import NavigationComponent from './Navigation/Navigation.vue'
+
+const route = useRoute()
 </script>
 
 <template>
 	<div class="main-layout">
 		<navigation-component />
 		<div class="layout-content">
+			<h1 class="text-3xl flex flex-col justify-center items-center">
+				{{ route.name }}
+			</h1>
 			<router-view />
 		</div>
 	</div>
